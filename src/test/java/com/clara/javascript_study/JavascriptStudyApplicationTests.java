2c23dfed053989b710f495d87b30215a1b9289c9
clara <clara@gccompany.co.kr>
2021-09-23 01:33:38 +0900

    initialize react + springboot

---
+++ src/test/java/com/clara/javascript_study/JavascriptStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clara.javascript_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavascriptStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
